a1d02bd3691ff81343f463c4de2d0641c75b3a11
Manuel <garcia.manuel.b7@gmail.com>
2021-07-15 11:37:05 -0500

    Now the text shows an elipsis when the text is greater than the available space

--- app/src/main/java/com/mnuel/dev/notes/ui/screens/note/EditNoteScreen.kt
+++ app/src/main/java/com/mnuel/dev/notes/ui/screens/note/EditNoteScreen.kt
@@ -27,6 +27,7 @@ import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -422,6 +423,8 @@ fun BottomAppBar(
                 Text(
                     text = selectedCollection.description,
                     style = MaterialTheme.typography.subtitle1,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
                 )
             }
         }
