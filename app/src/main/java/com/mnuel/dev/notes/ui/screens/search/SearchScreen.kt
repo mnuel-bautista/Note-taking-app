4e136fcb73e9ccf9bf215da91a21507c7911d515
Manuel <garcia.manuel.b7@gmail.com>
2021-07-17 13:16:25 -0500

    Move the placeholder and icon to the decoration box. so that the hit target area grows.

--- app/src/main/java/com/mnuel/dev/notes/ui/screens/search/SearchScreen.kt
+++ app/src/main/java/com/mnuel/dev/notes/ui/screens/search/SearchScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Close
@@ -13,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.unit.dp
 import com.mnuel.dev.notes.ui.screens.home.NoteListItem
 import com.mnuel.dev.notes.ui.screens.note.TextField
@@ -73,36 +75,39 @@ private fun SearchBar(
     onQueryChange: (query: String) -> Unit = {},
     onClose: () -> Unit = {},
 ) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(SearchBarHeight)
-            .clip(shape = RoundedCornerShape(4.dp))
-            .border(
-                width = 1.dp,
-                color = LocalContentColor.current.copy(alpha = 0.12f),
-                shape = RoundedCornerShape(4.dp)
-            ),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
-        Box(
-            modifier = Modifier
-                .fillMaxHeight()
-                .padding(start = 16.dp)
-                .weight(1f, fill = true),
-            contentAlignment = Alignment.CenterStart
-        ) {
-            TextField(
-                value = query,
-                onValueChange = { onQueryChange(it) },
-                placeholder = placeholder,
-                singleLine = true,
-                style = MaterialTheme.typography.body2
-            )
-        }
-        IconButton(onClick = onClose) {
-            Icon(imageVector = Icons.Outlined.Close, contentDescription = null)
-        }
-    }
+
+    BasicTextField(
+        value = query,
+        onValueChange = onQueryChange,
+        decorationBox = { innerTextField ->
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(SearchBarHeight)
+                    .clip(shape = RoundedCornerShape(4.dp))
+                    .border(
+                        width = 1.dp,
+                        color = LocalContentColor.current.copy(alpha = 0.12f),
+                        shape = RoundedCornerShape(4.dp)
+                    ),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Box(
+                    Modifier
+                        .weight(1f)
+                        .padding(start = 12.dp)
+                ) {
+                    innerTextField()
+                    if (query.isEmpty()) {
+                        Text(text = placeholder)
+                    }
+                }
+                IconButton(onClick = onClose) {
+                    Icon(imageVector = Icons.Outlined.Close, contentDescription = null)
+                }
+            }
+        },
+        textStyle = MaterialTheme.typography.subtitle1.copy(color = LocalContentColor.current),
+        cursorBrush = SolidColor(LocalContentColor.current)
+    )
 }
